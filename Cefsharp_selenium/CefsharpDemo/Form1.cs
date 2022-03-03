fad4b3015cf1da745f96c8c5bea97163fa3dfd27
nult <nult2003@gmail.com>
2022-03-03 17:07:03 +0700

    Add project

---
+++ Cefsharp_selenium/CefsharpDemo/Form1.cs
@@ -0,0 +1,95 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CefsharpDemo
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser browser;
+        public Form1()
+        {
+            InitializeComponent();
+            //InitCef();
+        }
+
+        private void InitCef()
+        {
+            CefSettings settings = new CefSettings();
+            settings.RemoteDebuggingPort = 9222;
+            settings.UserDataPath = "C:/Temp";
+            Cef.Initialize(settings);
+            var browserControl = new ChromiumWebBrowser("chrome://version");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var settings = new CefSettings();
+            settings.CefCommandLineArgs.Add("enable-npapi", "1");
+            settings.IgnoreCertificateErrors = true;
+            settings.RemoteDebuggingPort = 55555;
+            
+            //settings.CefCommandLineArgs.Add("enable-system-flash", "1");
+            Cef.Initialize(settings);
+            browser = new ChromiumWebBrowser("https://google.com.vn");        
+            browser.Dock = DockStyle.Fill;
+            browser.MenuHandler = new SearchContextMenuHandler();
+            this.Controls.Add(browser);
+        }
+    }
+    public class SearchContextMenuHandler : IContextMenuHandler
+    {
+        public void OnBeforeContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model)
+        {
+            model.Clear();
+            model.AddItem(CefMenuCommand.Back, "Go Back");
+            model.AddItem(CefMenuCommand.Reload, "Reload");
+            model.AddItem(CefMenuCommand.ReloadNoCache, "Reload (No Cache)");
+            model.AddItem(CefMenuCommand.Print, "Open DevTools");
+        }
+
+        public bool OnContextMenuCommand(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters,
+            CefMenuCommand commandId, CefEventFlags eventFlags)
+        {
+            if (commandId == CefMenuCommand.Reload)
+            {
+                browser.Reload();
+                return true;
+            }
+            if (commandId == CefMenuCommand.ReloadNoCache)
+            {
+                browser.Reload(true);
+                return true;
+            }
+            if (commandId == CefMenuCommand.Print)
+            {
+                CefSharp.WebBrowserExtensions.ShowDevTools(browser);
+                return true;
+            }
+            if (commandId == CefMenuCommand.Back)
+            {
+                browser.GoBack();
+                return true;
+            }
+
+            return false;
+        }
+
+        public void OnContextMenuDismissed(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+        {
+        }
+
+        public bool RunContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback)
+        {
+            return false;
+        }
+    }
+}
---
+++ Cefsharp_selenium/CefsharpDemoTest/Form1.cs
@@ -0,0 +1,102 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Windows.Forms;
+
+namespace CefsharpDemoTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btn_vidu1(object sender, EventArgs e)
+        {
+            Cef_trying3();
+        }
+
+        // not work
+        private void Cef_trying1()
+        {
+            try
+            {
+                var options = new ChromeOptions();
+                // cefclient.exe
+                //options.BinaryLocation = @"D:\my studies\study\CefsharpDemo\cef_binary_98.2.0+g78c653a+chromium-98.0.4758.102_windows64_client\Release\cefclient.exe";
+                options.BinaryLocation = @"C:\xProject\Study\Cefsharp_selenium\CefsharpDemo\bin\Debug\CefsharpDemo.exe";
+
+                //options.AddArgument("--log-level=3");
+                //options.AddArgument("--disable-extensions");
+                options.DebuggerAddress = "localhost:55555";// not work
+                var driver = new ChromeDriver(options); //chromedriver.exe
+
+                driver.Navigate().GoToUrl("http://stackoverflow.com/");
+                //driver.Manage().Window.Maximize();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // not work
+        private void Cef_trying2()
+        {
+            try
+            {
+                var options = new ChromeOptions();
+                // cefclient.exe
+                //options.BinaryLocation = @"D:\my studies\study\CefsharpDemo\cef_binary_98.2.0+g78c653a+chromium-98.0.4758.102_windows64_client\Release\cefclient.exe";
+                options.BinaryLocation = @"C:\xProject\Study\Cefsharp_selenium\CefsharpDemo\bin\Debug\CefsharpDemo.exe";
+                
+                //options.AddArgument("--log-level=3");
+                //options.AddArgument("--disable-extensions");
+                options.AddArgument("--remote-debugging-port=55555");// it works well
+                var service = ChromeDriverService.CreateDefaultService();
+                //service.HideCommandPromptWindow = true;                
+                service.Port = 55555;
+                var driver = new ChromeDriver(service, options); //chromedriver.exe
+
+                driver.Navigate().GoToUrl("http://stackoverflow.com/");
+                //driver.Manage().Window.Maximize();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // work well
+        private void Cef_trying3()
+        {
+            try
+            {
+                var options = new ChromeOptions();
+                // cefclient.exe
+                //options.BinaryLocation = @"D:\my studies\study\CefsharpDemo\cef_binary_98.2.0+g78c653a+chromium-98.0.4758.102_windows64_client\Release\cefclient.exe";
+                options.BinaryLocation = @"C:\xProject\Study\Cefsharp_selenium\CefsharpDemo\bin\Debug\CefsharpDemo.exe";
+
+                //options.AddArgument("--log-level=3");
+                //options.AddArgument("--disable-extensions");
+                options.AddArgument("--remote-debugging-port=55555");// it works well
+                var driver = new ChromeDriver(options); //chromedriver.exe
+
+                driver.Navigate().GoToUrl("https://staging.ibwave-cloud.com/");
+                //driver.Manage().Window.Maximize();
+                driver.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
